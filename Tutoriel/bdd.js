5b8ead2b474af3d304c1cbed95dcac8ec4f05bad
Alter3dByMalloc <alter3dbymalloc@outlook.com>
2026-06-11 09:35:50 +0200

    Update bdd.js

--- Tutoriel/bdd.js
+++ Tutoriel/bdd.js
@@ -1,8 +1,5 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-<script>
-  const { createClient } = supabase;
-  const client = createClient(
-    'https://dzjtugujayvwhibgrjvv.supabase.co',   // ta Project URL
-    'sb_publishable_UbB6RIfy9DRGccg4qiEOTA_ojNGsy0s'
-  );
-</script>
+const { createClient } = supabase;
+const client = createClient(
+  'https://dzjtugujayvwhibgrjvv.supabase.co',   // ta Project URL
+  'sb_publishable_UbB6RIfy9DRGccg4qiEOTA_ojNGsy0s'
+);
